d2d903040dc23f4ac22708925143c6ef02cb2c05
EVGENPC\evgen <evgenkitz@gmail.com>
2022-07-15 17:20:46 +0300

    Init commit: - added AX6000 support - added mapping overlay and opt partitions - added Russian: Chinese pages were translated into Russian - added ssh - fixed broken HTML in agreement_US_inter.htm

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# unpack, modify and re-pack the Xiaomi R3600 firmware
+# unpack, modify and re-pack the Xiaomi R6000 firmware
 # removes checks for release channel before starting dropbear
 #
 # 2020.07.20  darell tan
@@ -9,7 +9,8 @@
 set -e
 
 IMG=$1
-ROOTPW='$1$qtLLI4cm$c0v3yxzYPI46s28rbAYG//'  # "password"
+ROOTPW=''  # "password"
+SCRIPT_ROOT_DIR="$PWD"
 
 [ -e "$IMG" ] || { echo "rootfs img not found $IMG"; exit 1; }
 
@@ -18,6 +19,7 @@ command -v unsquashfs &>/dev/null || { echo "install unsquashfs"; exit 1; }
 mksquashfs -version >/dev/null || { echo "install mksquashfs"; exit 1; }
 
 FSDIR=`mktemp -d /tmp/resquash-rootfs.XXXXX`
+echo "FSDIR: $FSDIR"
 trap "rm -rf $FSDIR" EXIT
 
 # test mknod privileges
@@ -26,15 +28,23 @@ rm -f "$FSDIR/foo"
 
 >&2 echo "unpacking squashfs..."
 unsquashfs -f -d "$FSDIR" "$IMG"
+###############################################################################
 
 >&2 echo "patching squashfs..."
+
+# create /opt dir
+mkdir "$FSDIR/opt"
+chmod 755 "$FSDIR/opt"
+
+# create /layout dir
+mkdir "$FSDIR/layout"
 
 # modify dropbear init
 sed -i 's/channel=.*/channel=release2/' "$FSDIR/etc/init.d/dropbear"
 sed -i 's/flg_ssh=.*/flg_ssh=1/' "$FSDIR/etc/init.d/dropbear"
 
 # mark web footer so that users can confirm the right version has been flashed
-sed -i 's/romVersion%>/& xqrepack/;' "$FSDIR/usr/lib/lua/luci/view/web/inc/footer.htm"
+sed -i 's/romVersion%>/& evgenkitz/;' "$FSDIR/usr/lib/lua/luci/view/web/inc/footer.htm"
 
 # stop resetting root password
 sed -i '/set_user(/a return 0' "$FSDIR/etc/init.d/system"
@@ -65,40 +75,49 @@ NVRAM
 sed -i "s@root:[^:]*@root:${ROOTPW}@" "$FSDIR/etc/shadow"
 
 # stop phone-home in web UI
-cat <<JS >> "$FSDIR/www/js/miwifi-monitor.js"
-(function(){ if (typeof window.MIWIFI_MONITOR !== "undefined") window.MIWIFI_MONITOR.log = function(a,b) {}; })();
-JS
+#cat <<JS >> "$FSDIR/www/js/miwifi-monitor.js"
+#(function(){ if (typeof window.MIWIFI_MONITOR !== "undefined") window.MIWIFI_MONITOR.log = function(a,b) {}; })();
+#JS
 
 # add xqflash tool into firmware for easy upgrades
-cp xqflash "$FSDIR/sbin"
-chmod 0755      "$FSDIR/sbin/xqflash"
-chown root:root "$FSDIR/sbin/xqflash"
+# cp xqflash "$FSDIR/sbin"
+# chmod 0755      "$FSDIR/sbin/xqflash"
+# chown root:root "$FSDIR/sbin/xqflash"
 
 # dont start crap services
-for SVC in stat_points statisticsservice \
-		datacenter \
-		smartcontroller \
-		wan_check \
-		plugincenter plugin_start_script.sh cp_preinstall_plugins.sh; do
-	rm -f $FSDIR/etc/rc.d/[SK]*$SVC
-done
+#for SVC in stat_points statisticsservice \
+#		datacenter \
+#		smartcontroller \
+#		wan_check \
+#		plugincenter plugin_start_script.sh cp_preinstall_plugins.sh; do
+#	rm -f $FSDIR/etc/rc.d/[SK]*$SVC
+#done
 
 # prevent stats phone home & auto-update
-for f in StatPoints mtd_crash_log logupload.lua otapredownload wanip_check.sh; do > $FSDIR/usr/sbin/$f; done
+#for f in StatPoints mtd_crash_log logupload.lua otapredownload wanip_check.sh; do > $FSDIR/usr/sbin/$f; done
 
-rm -f $FSDIR/etc/hotplug.d/iface/*wanip_check
+# prevent auto-update
+> $FSDIR/usr/sbin/otapredownload
 
-sed -i '/start_service(/a return 0' $FSDIR/etc/init.d/messagingagent.sh
+#rm -f $FSDIR/etc/hotplug.d/iface/*wanip_check
+
+#sed -i '/start_service(/a return 0' $FSDIR/etc/init.d/messagingagent.sh
 
 # cron jobs are mostly non-OpenWRT stuff
-for f in $FSDIR/etc/crontabs/*; do
-	sed -i 's/^/#/' $f
-done
+#for f in $FSDIR/etc/crontabs/*; do
+#	sed -i 's/^/#/' $f
+#done
 
 # as a last-ditch effort, change the *.miwifi.com hostnames to localhost
-sed -i 's@\w\+.miwifi.com@localhost@g' $FSDIR/etc/config/miwifi
+#sed -i 's@\w\+.miwifi.com@localhost@g' $FSDIR/etc/config/miwifi
+
+if grep -q model=RA72 $FSDIR/usr/share/xiaoqiang/xiaoqiang-defaults.txt; then
+	echo "patch: $FSDIR/lib/preinit/90_mount_bind_etc"
+	patch $FSDIR/lib/preinit/90_mount_bind_etc "$SCRIPT_ROOT_DIR/patches/90_mount_bind_etc.patch"
+fi
+
+python translate/translate.py $FSDIR
 
 >&2 echo "repacking squashfs..."
 rm -f "$IMG.new"
 mksquashfs "$FSDIR" "$IMG.new" -comp xz -b 256K -no-xattrs
-
